c19ec4cf58ba80a91d4854ff4d51ef3c6e45f7a9
kuba <kelowej@gmail.com>
2020-06-10 19:56:07 +0200

    getOperatorFromUser

--- js/script.js
+++ js/script.js
@@ -4,17 +4,35 @@ function getNumberFromUser(){
     let input = prompt('Please, enter a number')
     let number = Number(input)
     if (isNaN(number)){
+        alert('It is not a number, please try again.')
         getNumberFromUser()
     }
     return number
 }
-function showResult(result){
+function showResult(result) {
     return alert('The result is ' + result)
 }
-function getOperatorFromUser(){}
-function getConfirmationFromUser(){}
-function getConfirmationFromUserAndProceed(){}
-function calculate(){}
+function getOperatorFromUser() {
+    const input = prompt('Enter an operator: +, -, *, /')
+    switch (input) {
+        case '+':
+            return input
+        case '-':
+            return input
+        case '*':
+            return input
+        case '/':
+            return input
+        default:
+            alert('Incorrect operator, please try again.')
+            getOperatorFromUser()
+    }
+
+
+}
+function getConfirmationFromUser() { }
+function getConfirmationFromUserAndProceed() { }
+function calculate() { }
 
 
 const number = getNumberFromUser()
