4b01fcbbf8b2bcfb3ff7b670f0c4dc150df3d216
kuba <kelowej@gmail.com>
2020-06-10 19:43:16 +0200

    getNumberFromUser

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict'
+
+function getNumberFromUser(){
+    let input = prompt('Please, enter a number')
+    let number = Number(input)
+    if (isNaN(number)){
+        getNumberFromUser()
+    }
+    return number
+}
+function showResult(result){}
+function getOperatorFromUser(){}
+function getConfirmationFromUser(){}
+function getConfirmationFromUserAndProceed(){}
+function calculate(){}
+
+
+const number = getNumberFromUser()
